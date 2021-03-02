ca29dc9cf3e2651c4e077d93e0f28a8f1b5f1413
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-02 19:54:07 +0800

    Add Navbar component
    Added gsap as a dependency to add animation on burger icon.
    The navbar links for mobile menu is not yet added.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // pages
 import { Campgrounds, CampgroundDetail } from "./pages";
 
+// components
+import { Navbar } from "./components";
+
+// styles
+import "./App.css";
+
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/campgrounds" component={Campgrounds}></Route>
         <Route
---
+++ src/components/Navbar.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import gsap from "gsap";
+
+const Navbar = () => {
+  const [burgerTl] = useState(gsap.timeline({ paused: true, duration: 0.2 }));
+
+  useEffect(() => {
+    burgerTl
+      .to(
+        ".burger__line--center",
+        {
+          opacity: 0,
+        },
+        0
+      )
+      .to(
+        ".burger__line--top",
+        {
+          y: "0.515625rem", // 8.25px
+        },
+        0
+      )
+      .to(
+        ".burger__line--bottom",
+        {
+          y: "-0.515625rem", // 8.25px
+        },
+        0
+      )
+      .to(
+        ".burger__line--top",
+        {
+          rotate: 45,
+          scaleX: 0.8,
+        },
+        0.2
+      )
+      .to(
+        ".burger__line--bottom",
+        {
+          rotate: -45,
+          scaleX: 0.8,
+        },
+        0.2
+      )
+      .reverse();
+  }, []);
+
+  const toggleBurgerTimeline = () => {
+    if (burgerTl.reversed()) {
+      burgerTl.timeScale(2).play();
+    } else {
+      burgerTl.timeScale(2).reverse();
+    }
+  };
+
+  return (
+    <header className="header">
+      <nav className="container">
+        <div className="row">
+          <div className="col col1">
+            <span className="logo">YelpCamp</span>
+          </div>
+          <div className="col col2 nav-links-desktop">
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/campgrounds">Campgrounds</a>
+              </li>
+              <li>
+                <a href="/">New Campgrounds</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col col3 burger-button">
+            <button onClick={toggleBurgerTimeline} className="burger">
+              <div className="burger__line burger__line--top"></div>
+              <div className="burger__line burger__line--center"></div>
+              <div className="burger__line burger__line--bottom"></div>
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
